b6f91126df430462ea3bc173cffc7a7482fadc26
mkiw1464-debug <mkiw1464@gmail.com>
2026-09-06 04:48:31 +0800

    Add files via upload

---
+++ fishhook/fishhook.h
@@ -0,0 +1,24 @@
+#ifndef fishhook_h
+#define fishhook_h
+
+#include <stddef.h>
+#include <stdint.h>
+
+#if !defined(FISHHOOK_EXPORT)
+#define FISHHOOK_EXPORT extern
+#endif
+
+struct rebinding {
+    const char *name;
+    void *replacement;
+    void **replaced;
+};
+
+FISHHOOK_EXPORT int rebind_symbols(struct rebinding rebindings[],
+                                   size_t rebindings_nel);
+
+FISHHOOK_EXPORT int rebind_symbols_image(void *header,
+                                         intptr_t slide,
+                                         struct rebinding rebindings[],
+                                         size_t rebindings_nel);
+#endif
